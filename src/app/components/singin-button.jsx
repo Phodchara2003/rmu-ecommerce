6ba21c2819e5c6651d485afa957663f6ecebb3b9
Phodchara2003 <653170010122@rmu.ac.th>
2025-02-18 22:06:44 +0700

    nextjs

---
+++ src/app/components/singin-button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SinginButton = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">Sign in</button>
+    </div>
+  )
+}
+
+export default SinginButton
